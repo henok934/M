76fb231defc883a0a4b1a3eb2937983d6a23ac41
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2025-04-02 11:13:57 -0400

    fix

--- myproje/myproje/settings.py
+++ myproje/myproje/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'myproje.wsgi.application'
 
 #DATABASES = {
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -78,19 +78,24 @@ DATABASES = {
     }
 }
 """
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'my_db',
+        'NAME': 'my_app_db',  # Use your actual database name
+        #'NAME': 'my_db',
         'USER': 'data',  # Your PostgreSQL username
         'PASSWORD': 'Henok@934',  # Your PostgreSQL password
+        'HOST': 'my_db.oregon.render.com',  # Use your actual Render host
         #'HOST': 'my_db.oregon.render.com',
         #'HOST': 'data.oregon.render.com',  # Replace with the actual host
-        'HOST': '127.0.0.1',  # Set to empty string for default
+        #'HOST': '127.0.0.1',  # Set to empty string for default
+        #'HOST': 'db.my_db.<region>.render.com',  # Replace <region> with your actual region
         'PORT': '5432',  # Set to empty string for default
+        'HOST': 'my_app_db.oregon.render.com',  # Use your actual Render host
     }
 }
-
+"""
 
 
 AUTH_USER_MODEL = 'users.CustomUser'  # Replace with your app name
